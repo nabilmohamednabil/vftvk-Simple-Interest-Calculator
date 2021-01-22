182a76d014cd9a547bd82690608b0c6c9baef6f3
nabilmohamednabil <nabilmohamed997@gmail.com>
2021-01-22 18:10:13 +0000

    saving change to the simple calculator

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+var AmountElement = document.getElementById("Amount");
+var amount = document.getElementById("Amount").value;
+if( amount=='' || amount <= 0){
+    window.alert("Enter a positive number");
+    AmountElement.focus();
+    AmountElement.value = '' ;
+    return false ;
+       }
+var rate = document.getElementById("Interest Rate").value;
+var years = document.getElementById("No. of Years").value;
+var realinterest = parseInt((amount * years * (rate / 100)));  
+var currentdate = new Date();
+var y = parseInt(currentdate.getFullYear()) + parseInt(years)   ; 
+document.getElementById("result").innerHTML = 
+  '<p>If you deposit '+amount+'<br/>'+'at an interest rate of '+rate+'%<br/>'+'You will receive an amount of '+realinterest+'<br/>'+'in the year '+y+'<br/></p>' ; 
 }
-        
+
+function onsliderchange(){
+    var rate = document.getElementById("Interest Rate").value;
+    document.getElementById("slidder").innerHTML = rate + '%' ;
+
+}
